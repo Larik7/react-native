1d2fc708fa5543def1104c06cb0a21ac89fbcc75
Larik7 <larik777taganka@gmail.com>
2023-07-19 16:27:24 +0300

    add register form, and BImg

---
+++ Screens/RegistrationScreen.js
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View, TextInput, ImageBackground } from "react-native";
+
+export function RegisterForm () {
+    return (
+        <View style={styles.container}>
+            <ImageBackground style={styles.imageBg} source={require('../images/Photo-BG.jpg')}>
+            <View>
+                <Text>Register</Text>
+                <View>
+                    <TextInput style={styles.input} placeholder="Login"/>
+                    <TextInput style={styles.input} placeholder="Email"/>
+                    <TextInput style={styles.input} placeholder="Password"/>
+                </View>
+                <TouchableOpacity style={styles.registrationBtn}>
+                    <Text style={styles.registrationTitle}>Registration</Text>
+                </TouchableOpacity>
+            </View>
+            </ImageBackground>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff"
+    },
+    imageBg: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
